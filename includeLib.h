ae5c1759bbd99270bb68490637db11d4b2ae1d1c
Chase <w3history@empal.com>
2016-12-19 22:39:02 +0900

    name 수정

--- includeLib.h
+++ includeLib.h
@@ -1,23 +1,23 @@
 #pragma once
 
-#include <CFCNet/net/include/CFCNet_service.h>
-#include <CFCNet/net/include/connection.h>
+#include <Net/net/include/netService.h>
+#include <Net/net/include/connection.h>
 
 
 #ifdef _WIN64
 
 #ifdef _DEBUG
-#pragma comment(lib, "CFCNet/lib64/CFCNet_x64D.lib")
+#pragma comment(lib, "Net/lib64/Net_x64D.lib")
 #else
-#pragma comment(lib, "CFCNet/lib64/CFCNet_x64.lib")
+#pragma comment(lib, "Net/lib64/Net_x64.lib")
 #endif
 
 #elif _WIN32
 
 #ifdef _DEBUG
-#pragma comment(lib, "CFCNet/lib/CFCNetD.lib")
+#pragma comment(lib, "Net/lib/NetD.lib")
 #else
-#pragma comment(lib, "CFCNet/lib/CFCNet.lib")
+#pragma comment(lib, "Net/lib/Net.lib")
 #endif
 
 #endif
