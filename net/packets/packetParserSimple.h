7e88b7ab9b4e1fcbaf30ef193ca9ef9ced5b5d66
chaseYLC <w3history@empal.com>
2020-12-30 23:53:53 +0900

    packetParserSimple

--- net/packets/packetParserSimple.h
+++ net/packets/packetParserSimple.h
@@ -13,55 +13,57 @@
 #define SIMPLE_PARSER_KEY_GETKEY(key)			(key & 0xffff0000)
 #define SIMPLE_PARSER_KEY_GETLENGTH(key)		(key & 0x0000ffff)
 
-struct SimpleParserProposal
-{
-	enum{
-		MAX_PACKET = 2048,
-		PARTKEY_SIZE = 4,
-		HEADER_SIZE = PARTKEY_SIZE + PARTKEY_SIZE,
-	};
-};
-
-class message_header_manipulator_simpleParser
-{
-public:
-	struct HEADER
+namespace mln {
+	struct SimpleParserProposal
 	{
-		int32_t mainKey;
-		int32_t partKey;
+		enum {
+			MAX_PACKET = 2048,
+			PARTKEY_SIZE = 4,
+			HEADER_SIZE = PARTKEY_SIZE + PARTKEY_SIZE,
+		};
 	};
 
-	static size_t getHeaderSize()
+	class message_header_manipulator_simpleParser
 	{
-		return sizeof(HEADER);
-	}
+	public:
+		struct HEADER
+		{
+			int32_t mainKey;
+			int32_t partKey;
+		};
 
-	static void facilitate(size_t currentSize, char *buffer)
+		static size_t getHeaderSize()
+		{
+			return sizeof(HEADER);
+		}
+
+		static void facilitate(size_t currentSize, char* buffer)
+		{
+			const int32_t  bodySize = (int32_t)(currentSize - sizeof(HEADER));
+
+			((HEADER*)buffer)->mainKey = SIMPLE_PARSER_KEY_MAKEKEY(SIMPLE_PARSER_MAIN_KEY, SimpleParserProposal::PARTKEY_SIZE + bodySize);
+			((HEADER*)buffer)->partKey = SIMPLE_PARSER_KEY_MAKEKEY(SIMPLE_PARSER_PART_KEY, bodySize);
+		}
+	};
+
+	class SIMPLE_PACKET_PARSING_SUPPORT
 	{
-		const int32_t  bodySize = (int32_t)(currentSize - sizeof(HEADER));
+	public:
+		static mln::MsgUserManip* getMsgManipulator();
 
-		((HEADER*)buffer)->mainKey = SIMPLE_PARSER_KEY_MAKEKEY(SIMPLE_PARSER_MAIN_KEY, SimpleParserProposal::PARTKEY_SIZE + bodySize);
-		((HEADER*)buffer)->partKey = SIMPLE_PARSER_KEY_MAKEKEY(SIMPLE_PARSER_PART_KEY, bodySize);
-	}
-};
+		static bool packetParser(mln::Connection::sptr spConn, mln::CircularStream::Ptr msg
+			, mln::MessageProcedure& msg_proc
+			, mln::MessageProcedure::msgMapTy& memberFuncMap
+			, mln::MessageProcedure::msgMapTy& staticFuncMap);
 
-class SIMPLE_PACKET_PARSING_SUPPORT
-{
-public:
-	static mln::MsgUserManip* getMsgManipulator();
+		static bool packetParser_notUseEnc(mln::Connection::sptr spConn, mln::CircularStream::Ptr msg
+			, mln::MessageProcedure& msg_proc
+			, mln::MessageProcedure::msgMapTy& memberFuncMap
+			, mln::MessageProcedure::msgMapTy& staticFuncMap);
 
-	static bool packetParser(mln::Connection::sptr spConn, mln::CircularStream::Ptr msg
-		, mln::MessageProcedure &msg_proc
-		, mln::MessageProcedure::msgMapTy& memberFuncMap
-		, mln::MessageProcedure::msgMapTy& staticFuncMap);
-
-	static bool packetParser_notUseEnc(mln::Connection::sptr spConn, mln::CircularStream::Ptr msg
-		, mln::MessageProcedure &msg_proc
-		, mln::MessageProcedure::msgMapTy& memberFuncMap
-		, mln::MessageProcedure::msgMapTy& staticFuncMap);
-
-	static bool packetParser_performanced(mln::Connection::sptr spConn, mln::CircularStream::Ptr msg
-		, mln::MessageProcedure &msg_proc
-		, mln::MessageProcedure::msgMapTy& memberFuncMap
-		, mln::MessageProcedure::msgMapTy& staticFuncMap);
-};
+		static bool packetParser_performanced(mln::Connection::sptr spConn, mln::CircularStream::Ptr msg
+			, mln::MessageProcedure& msg_proc
+			, mln::MessageProcedure::msgMapTy& memberFuncMap
+			, mln::MessageProcedure::msgMapTy& staticFuncMap);
+	};
+}//namespace mln {
